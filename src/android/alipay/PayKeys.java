3be649a8ca4a82aa03395df1ee62ad124648fd4f
hpduan <hpduan@jingqubao>
2016-04-07 16:03:20 +0800

    rm

--- src/android/alipay/PayKeys.java
+++ src/android/alipay/PayKeys.java
@@ -9,15 +9,15 @@ package com.yumemor.cordova.alipay;
 public final class PayKeys {
 
 	// 合作身份者id，以2088开头的16位纯数字
-	public static final String DEFAULT_PARTNER = "2088501323901268";
+	public static final String DEFAULT_PARTNER = "";
 
 	// 收款支付宝账号
-	public static final String DEFAULT_SELLER = "heroghost1000@hotmail.com";
+	public static final String DEFAULT_SELLER = DEFAULT_PARTNER;
 
 	// 商户私钥 自助生成
-	public static final String PRIVATE = "MIICXgIBAAKBgQDcHngHQTB4bRGaU6kzzjjW4vj4C6++wufh7KoAMJa2GkuIlF7GB012DaWilzZEr7L1D/vTWI5LGhnxKbTEf07PJYAQUDi6G6HRXqizL9K1PfD51bQtJ279qnioLZCOIhfdAUcYaPOkP1ADnBXb5kLb2e3FZBOC1uJIpZN6cxfqcwIDAQABAoGBAM1e+Cn8npadXKEakXGuZMXJOvVjIfPfsa3Ou8Y1j0gUPIkPtWUaByGwFewfHRj0vQl8gj5sD0bu8Wlaujfjd8h+Ga3XYuxyNcwDGDynHvkV2r8oqaph/DoqYzCMWJ3kftJNq8LSbFU0+FZXKEKA3WQ0I8AJLT1ZqhxKuXS7JNARAkEA9+SfA2WGFu5nT7aK/sa6CZ0Stvt08TSROSejB1t1qwSaFW6hqHWeDxS82pf/V/o7eMcjPa7rAnUjH5Od+LSmHwJBAONRUkwcLZdaAI7j9O6sI3GJuy3KTKG/nmEIbVuxGsnNLDXmT7kEtBk3CLG9H79k5iPodczxY75SpRr/L25MaS0CQG+/AKuATZTyvWGisT+uh9ijlPclnY7KGpkANC6x3PZN6cO1jmwpMSotOAD2OeYH6F1Gr60zt77w3Pd0DOc7UBMCQQCZPlhhsNlXYDhiG/Eh+XAHeZdzHh8dJ8XImlrG5pUtmCCG0ZXLIA2dtgkpcVr2tzmi2XKYz96zDpn5ojDChcadAkEAk+y2TtjlKmfKU9aZ0uzX5DdgMRdVM5gMym0qO0+ulTo3cdVqwCNcwJJuJzGO5ZWdACgATejAjxoXPvmyru7sRQ==";
+	public static final String PRIVATE = "";
 
 	// 支付宝默认公钥 请勿修改
-	public static final String PUBLIC = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDcHngHQTB4bRGaU6kzzjjW4vj4C6++wufh7KoAMJa2GkuIlF7GB012DaWilzZEr7L1D/vTWI5LGhnxKbTEf07PJYAQUDi6G6HRXqizL9K1PfD51bQtJ279qnioLZCOIhfdAUcYaPOkP1ADnBXb5kLb2e3FZBOC1uJIpZN6cxfqcwIDAQAB";
+	public static final String PUBLIC = "":
 
 }
